e2a1a5b0fc7f0d5b45a8dbbc7754cba499a876f5
Andrewwango <andrew.wang27@gmail.com>
2025-03-10 23:42:50 +0000

    weighted SSDU ablations

--- train_paper.py
+++ train_paper.py
@@ -227,7 +227,20 @@ match args.loss:
         split_generator = dinv.physics.generator.GaussianMaskGenerator(img_size=(320, 320), acceleration=2, rng=rng, device=device)
         mask_generator = dinv.physics.generator.MultiplicativeSplittingMaskGenerator((1, 320, 320), split_generator)
         loss = dinv.loss.WeightedSplittingLoss(mask_generator=mask_generator, physics_generator=physics_generator)
-    
+    case "weighted-ssdu-ablation-1":
+        split_generator = dinv.physics.generator.GaussianMaskGenerator(img_size=(320, 320), acceleration=2, rng=rng, device=device)
+        mask_generator = dinv.physics.generator.GaussianSplittingMaskGenerator((2, 320, 320), split_ratio=0.6, device=device, rng=rng)
+        loss = dinv.loss.WeightedSplittingLoss(mask_generator=mask_generator, physics_generator=physics_generator)
+    case "weighted-ssdu-ablation-2":
+        split_generator = dinv.physics.generator.GaussianMaskGenerator(img_size=(320, 320), acceleration=2, rng=rng, device=device)
+        mask_generator = dinv.physics.generator.MultiplicativeSplittingMaskGenerator((1, 320, 320), split_generator)
+        loss = dinv.loss.SplittingLoss(mask_generator=mask_generator, eval_split_input=False)
+    case "weighted-ssdu-ablation-3":
+        split_generator = dinv.physics.generator.GaussianMaskGenerator(img_size=(320, 320), acceleration=2, rng=rng, device=device)
+        mask_generator = dinv.physics.generator.MultiplicativeSplittingMaskGenerator((1, 320, 320), split_generator)
+        loss = dinv.loss.SplittingLoss(mask_generator=mask_generator, eval_split_input=True, eval_n_samples=3)
+
+
     case "ei-sure":
         loss = [
             dinv.loss.SureGaussianLoss(sigma=0.),
