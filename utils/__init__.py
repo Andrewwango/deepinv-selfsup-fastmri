78e4e961fd307fc604a93c9508adaae2cd7278d1
Andrewwango <andrew.wang27@gmail.com>
2025-03-29 23:26:04 +0000

    fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -50,4 +50,5 @@ class SimulatedLocalDataset(LocalDataset):
         params = {"mask" : params["mask"]} #discard coil_maps
         physics = MultiCoilMRI(img_size=x.shape[-2:], coil_maps=None, **params)
         y = physics(x.unsqueeze(0)).squeeze(0)
+        params["coil_maps"] = physics.coil_maps
         return x, y, params
